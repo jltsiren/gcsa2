11ee0ba2bb1e57ebc7634a223196b5af4c1cbcb8
Jouni Siren <jouni.siren@iki.fi>
2018-02-23 14:37:04 -0800

    Version 1.1

--- include/gcsa/utils.h
+++ include/gcsa/utils.h
@@ -150,7 +150,7 @@ struct Version
   static void print(std::ostream& out, const std::string& tool_name, bool verbose = false, size_type new_lines = 2);
 
   const static size_type MAJOR_VERSION = 1;
-  const static size_type MINOR_VERSION = 0;
+  const static size_type MINOR_VERSION = 1;
   const static size_type PATCH_VERSION = 0;
 
   const static size_type GCSA_VERSION  = 3;
